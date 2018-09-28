1f510620413fbfddc3273e12e57ed3eacdad4b4f
Daniela <dvilchezs@ulasalle.edu.pe>
2018-09-28 11:15:17 -0500

    tt4

---
+++ laboratorio 2/try-this04.cpp
@@ -0,0 +1,22 @@
+//imprimir numeros y letras mayusculas con su codigo ascci
+#include <iostream>
+using namespace std;
+int main(){
+    char x=64;
+    int y=63;
+    for(y++;y<91;){
+        cout<<x<<" "<<y<<"\n";
+        x++;
+        y++;
+    }
+    char n=48;
+    int i=47;
+    for(i++;i<58;){
+        cout<<n<<" "<<i<<"\n";
+        n++;
+        i++;
+    }
+
+}
+
+
