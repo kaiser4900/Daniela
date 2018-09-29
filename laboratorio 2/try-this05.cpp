547545c614e544576645c67798a8a3fe10f3bedb
Daniela <dvilchezs@ulasalle.edu.pe>
2018-09-28 20:34:56 -0500

    tt 05

--- laboratorio 2/try-this05.cpp
+++ laboratorio 2/try-this05.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
-void square(){
-  int x;
-  cin >>x;
+void square(int x){
   int y=0;
   int p=0;
   while (y<x){
@@ -12,5 +10,7 @@ void square(){
   cout<<"resultado de la potencia al cuadrado:  "<<p<<"\n";
 }
 int main(){
-  square();
+  int x;
+  cin >>x;
+  square(x);
 }
