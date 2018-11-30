deac10956b8f6f3137f65298aa98d5aa0753fc33
Daniela <dvilchezs@ulasalle.edu.pe>
2018-11-30 13:53:31 -0500

    lab 06

---
+++ laboratorio 6/code03.cpp
@@ -0,0 +1,8 @@
+int main ()
+{
+int n=7;
+int* pi = new int; // a s i g n a un i n t
+int *qi = new int[4]; // a s i g n a 4 i n t s
+double * pd = new double; // a s i g n a un d o u b l e
+double * qd = new double[n]; // a s i g n a n d o u b l e s
+// Un a r r a y de n d o u b l e s
