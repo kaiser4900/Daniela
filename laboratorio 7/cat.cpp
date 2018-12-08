2ae61e48bab9158d6786357c1ff00670466b5db4
Daniela <dvilchezs@ulasalle.edu.pe>
2018-12-07 19:52:35 -0500

    lab 07

---
+++ laboratorio 7/cat.cpp
@@ -0,0 +1,21 @@
+#include "cat.h"
+#include <iostream>
+using namespace std:
+cat::cat(int _age, string _name):Animal(_age, _name)
+{
+
+}
+cat::~cat()
+{
+
+}
+void cat::speak()
+{
+    cout<<"miau!!"<<endl;
+}
+
+ostream & operator <<(ostream & os, const cat & a)
+{
+    os<< "Cat: "<<a.name<<" - "<<a.age;
+    return os;
+}
